2ddc6eb728e1de656844284fdc59776fcb065a6e
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-02 09:39:08 +0200

    Add simple navigation

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 export default function Movies() {
   return (
     <main>
+      <nav className="flex gap-2 p-2">
+        <Link href="/">Home</Link>
+        <Link href="/music">Music</Link>
+        <Link href="/movies">Movies</Link>
+        <Link href="/games">Games</Link>
+        <Link href="/relax">Regax</Link>
+        <Link href="/settings">Settings</Link>
+      </nav>
       <h1>Movies</h1>
     </main>
   );
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 export default function Settings() {
-    return (
-      <main>
-        <h1>Settings</h1>
-      </main>
-    );
-  }
-  
+  return (
+    <main>
+      <nav className="flex gap-2 p-2">
+        <Link href="/">Home</Link>
+        <Link href="/music">Music</Link>
+        <Link href="/movies">Movies</Link>
+        <Link href="/games">Games</Link>
+        <Link href="/relax">Regax</Link>
+        <Link href="/settings">Settings</Link>
+      </nav>
+      <h1>Settings</h1>
+    </main>
+  );
+}
