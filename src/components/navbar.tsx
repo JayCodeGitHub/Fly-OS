b06d3f8a5f43deb955f2f962893f097f64b0f543
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-02 10:14:18 +0200

    Move NavigationItems to separate file

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,10 @@
 import Link from "next/link";
-
-const navigationItems = [
-  { name: "Home", path: "/" },
-  { name: "Music", path: "/music" },
-  { name: "Movies", path: "/movies" },
-  { name: "Games", path: "/games" },
-  { name: "Relax", path: "/relax" },
-  { name: "Settings", path: "/settings" },
-];
+import { NavigationItems } from "@/items/navigationItems";
 
 export default function NavBar() {
   return (
     <nav className="fixed top-0 left-0 py-48 w-28 p-2 h-screen flex flex-col justify-around items-center">
-      {navigationItems.map((item) => (
+      {NavigationItems.map((item) => (
         <Link key={item.path} href={item.path}>
           {item.name}
         </Link>
---
+++ src/items/navigationItems.ts
@@ -0,0 +1,8 @@
+export const NavigationItems = [
+  { name: "Home", path: "/" },
+  { name: "Music", path: "/music" },
+  { name: "Movies", path: "/movies" },
+  { name: "Games", path: "/games" },
+  { name: "Relax", path: "/relax" },
+  { name: "Settings", path: "/settings" },
+];
