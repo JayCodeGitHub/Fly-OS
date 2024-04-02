760b16bfc2e6a2a366092300233693ffd8125728
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-02 09:45:59 +0200

    Move navigation to separate component

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,16 +1,10 @@
 import Link from "next/link";
+import NavBar from "@/components/navbar";
 
 export default function Movies() {
   return (
     <main>
-      <nav className="flex gap-2 p-2">
-        <Link href="/">Home</Link>
-        <Link href="/music">Music</Link>
-        <Link href="/movies">Movies</Link>
-        <Link href="/games">Games</Link>
-        <Link href="/relax">Regax</Link>
-        <Link href="/settings">Settings</Link>
-      </nav>
+      <NavBar />
       <h1>Movies</h1>
     </main>
   );
---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+      <nav className="flex gap-2 p-2">
+        <Link href="/">Home</Link>
+        <Link href="/music">Music</Link>
+        <Link href="/movies">Movies</Link>
+        <Link href="/games">Games</Link>
+        <Link href="/relax">Regax</Link>
+        <Link href="/settings">Settings</Link>
+      </nav>
+  );
+}
