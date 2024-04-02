8be8520d9b7c379e2c95acd5ea3f871de5e80afe
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-02 10:11:25 +0200

    Refactore navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 
+const navigationItems = [
+  { name: "Home", path: "/" },
+  { name: "Music", path: "/music" },
+  { name: "Movies", path: "/movies" },
+  { name: "Games", path: "/games" },
+  { name: "Relax", path: "/relax" },
+  { name: "Settings", path: "/settings" },
+];
+
 export default function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 py-48 w-28 p-2 h-screen flex flex-col justify-around items-center gap-2">
-      <Link href="/">Home</Link>
-      <Link href="/music">Music</Link>
-      <Link href="/movies">Movies</Link>
-      <Link href="/games">Games</Link>
-      <Link href="/relax">Relax</Link>
-      <Link href="/settings">Settings</Link>
+    <nav className="fixed top-0 left-0 py-48 w-28 p-2 h-screen flex flex-col justify-around items-center">
+      {navigationItems.map((item) => (
+        <Link key={item.path} href={item.path}>
+          {item.name}
+        </Link>
+      ))}
     </nav>
   );
 }
