88248d4ce9eb29692e20c0ce0e7eeb1d7d84e3b1
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-03 10:10:10 +0200

    Remove unused google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import NavBar from "@/components/navbar";
 import Toolbar from "@/components/toolbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NavBar />
         <Toolbar />
         {children}
