3490a510694527d006baf48f1b719eecdd8c3a21
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-03 09:24:25 +0200

    Add active class name and hover to navItem

--- src/components/navItem.tsx
+++ src/components/navItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavItem({
   icon,
@@ -12,20 +13,21 @@ export default function NavItem({
   color: string;
 }) {
   const colors = [
-    { name: "purple", color: "bg-purple-500" },
-    { name: "blue", color: "bg-blue-500" },
-    { name: "orange", color: "bg-orange-500" },
-    { name: "yellow", color: "bg-yellow-500" },
-    { name: "sky", color: "bg-sky-500" },
-    { name: "gray", color: "bg-gray-500" },
+    { name: "purple", color: "bg-purple-500", shadow: "shadow-purple-500" },
+    { name: "blue", color: "bg-blue-500", shadow: "shadow-blue-500" },
+    { name: "orange", color: "bg-orange-500", shadow: "shadow-orange-500" },
+    { name: "yellow", color: "bg-yellow-500", shadow: "shadow-yellow-500" },
+    { name: "sky", color: "bg-sky-500", shadow: "shadow-sky-500" },
+    { name: "gray", color: "bg-gray-500", shadow: "shadow-gray-500" },
   ];
-
+  const currentRoute = usePathname();
   return (
     <Link
       href={href}
-      className={`w-16 h-16 flex justify-center items-center rounded-lg text-white ${
-        colors.find((c) => c.name === color)?.color
-      }`}
+      className={`w-16 h-16 flex justify-center items-center rounded-lg text-white hover:shadow-2xl transition-all duration-300 ease-in-out
+      ${colors.find((c) => c.name === color)?.color} 
+      ${colors.find((c) => c.name === color)?.shadow}
+      ${currentRoute === href ? `shadow-2xl` : "shadow-none"}`}
     >
       {icon}
     </Link>
