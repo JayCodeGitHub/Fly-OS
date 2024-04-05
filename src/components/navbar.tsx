4994c5a46973318b2cf017dbbd1e42272ec77033
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-05 08:00:00 +0200

    Fix NavBar padding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import NavItem from "./navItem";
 
 export default function NavBar() {
   return (
-    <nav className="fixed lg:top-0 bottom-0 left-0 lg:py-48 lg:w-28 w-screen p-2 lg:h-screen flex lg:flex-col flex-row justify-around lg:items-center">
+    <nav className="fixed lg:top-0 bottom-0 left-0 lg:py-48 lg:w-28 w-screen p-4 lg:p2 lg:h-screen flex lg:flex-col flex-row justify-around lg:items-center">
       {NavigationItems.map((item) => (
         <NavItem
           key={item.path}
