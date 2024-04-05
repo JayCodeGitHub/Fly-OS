0ccdeeb9e19048d2c282a2f4ee74a3053b0959f6
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-05 10:05:46 +0200

    Add active animation to ValumeSlider

--- src/components/volumeSlider.tsx
+++ src/components/volumeSlider.tsx
@@ -1,17 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import { SpeakerLoudIcon, SpeakerOffIcon } from "@radix-ui/react-icons";
 import * as Slider from "@radix-ui/react-slider";
 import useStore from "@/state";
+import { motion } from "framer-motion";
 
 export default function VolumeSlider() {
+  const [isActive, setIsActive] = useState(false);
+  const toggleActive = () => setIsActive((prevValue) => !prevValue);
   const { volume, setVolume } = useStore((state) => state);
+
   return (
-    <div className="flex gap-4 items-center">
-      <SpeakerOffIcon />
-      <form className=" focus:outline outline-2">
+    <div className="flex gap-4 items-center p-1">
+      <SpeakerOffIcon className={`${isActive ? " visible" : "hidden"}`} />
+      <motion.form
+        animate={isActive ? { width: 200 } : { width: 0 }}
+        transition={{ duration: 0.2 }}
+        className="focus:outline outline-2"
+      >
         <Slider.Root
-          className="relative flex items-center select-none touch-none w-[200px] h-5"
+          className="relative flex items-center select-none touch-none h-5 w-full"
           defaultValue={[volume]}
           onValueChange={(values) => setVolume(values[0])}
           max={100}
@@ -21,12 +30,16 @@ export default function VolumeSlider() {
             <Slider.Range className="absolute bg-white rounded-full h-full" />
           </Slider.Track>
           <Slider.Thumb
-            className="block w-3 h-3 bg-white rounded-[10px] hover:bg-gray-200 transition-all focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA3 focus:bg-gray-200"
+            className={`block w-3 h-3 bg-white rounded-[10px] hover:bg-gray-300 transition-all focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA3 focus:bg-gray-300 ${
+              isActive ? "visible" : "hidden"
+            }`}
             aria-label="Volume"
           />
         </Slider.Root>
-      </form>
-      <SpeakerLoudIcon />
+      </motion.form>
+      <button onClick={toggleActive}>
+        <SpeakerLoudIcon />
+      </button>
     </div>
   );
 }
