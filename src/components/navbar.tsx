ca74b9aa461c65e654b36624eea14381b5274103
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-05 07:57:29 +0200

    Style NavBar for Mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import NavItem from "./navItem";
 
 export default function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 py-48 w-28 p-2 h-screen flex flex-col justify-around items-center">
+    <nav className="fixed lg:top-0 bottom-0 left-0 lg:py-48 lg:w-28 w-screen p-2 lg:h-screen flex lg:flex-col flex-row justify-around lg:items-center">
       {NavigationItems.map((item) => (
         <NavItem
           key={item.path}
