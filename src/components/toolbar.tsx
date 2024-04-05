7b872f6e05e428378153c75878203c6d7b369a92
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-05 08:09:17 +0200

    Create VolumeSlider component

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,7 +1,9 @@
+import VolumeSlider from "./volumeSlider";
+
 export default function Toolbar() {
   return (
     <div className="fixed top-0 left-0 h-28 px-48 w-screen flex justify-end items-center gap-12">
-      <h1>Toolbar</h1>
+      <VolumeSlider />
     </div>
   );
 }
---
+++ src/components/volumeSlider.tsx
@@ -0,0 +1,7 @@
+export default function VolumeSlider() {
+  return (
+    <div>
+      <h1>Volume Slider</h1>
+    </div>
+  );
+}
