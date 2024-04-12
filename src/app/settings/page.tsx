ef32ad9534d4bba197b466b9826d8b09c3573c8b
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-12 15:27:20 +0200

    Style settings page section

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,9 +2,11 @@ import Switch from "@/components/switch";
 
 export default function Settings() {
   return (
-    <main className="mx-28 mt-28 flex justify-center items-center h-restH">
-      <h1>Settings</h1>
-      <Switch>Dark Mode</Switch>
+    <main className="mx-28 mt-28 pt-36 flex justify-center items-start h-restH">
+      <section className="max-w-2xl w-full rounded-xl p-4 flex flex-col gap-4 bg-gray-600 shadow-lg shadow-gray-700">
+        <h1 className=" text-xl text-center">Settings</h1>
+        <Switch>Dark Mode</Switch>
+      </section>
     </main>
   );
 }
