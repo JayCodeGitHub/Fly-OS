c9d2929fb1b9b93266b07c9d8b1f88ffddd7c927
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-12 13:38:27 +0200

    Style switch component

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,10 @@
+import Switch from "@/components/switch";
+
 export default function Settings() {
   return (
     <main className="ml-28 mt-28">
       <h1>Settings</h1>
+      <Switch>Dark Mode</Switch>
     </main>
   );
 }
