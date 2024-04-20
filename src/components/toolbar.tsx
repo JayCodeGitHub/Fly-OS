18deccbee06ec92088dadc47a5869ab74bc34278
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-20 08:46:19 +0200

    Add conditional hiding to volumeSlider

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -3,7 +3,7 @@ import VolumeSlider from "./volumeSlider";
 export default function Toolbar() {
   return (
     <div className="fixed top-0 left-0 h-28 px-48 w-screen flex justify-end items-center gap-12">
-      <VolumeSlider />
+      <VolumeSlider hiding />
     </div>
   );
 }
--- src/components/volumeSlider.tsx
+++ src/components/volumeSlider.tsx
@@ -6,14 +6,16 @@ import * as Slider from "@radix-ui/react-slider";
 import useStore from "@/state";
 import { motion } from "framer-motion";
 
-export default function VolumeSlider() {
-  const [isActive, setIsActive] = useState(false);
-  const toggleActive = () => setIsActive((prevValue) => !prevValue);
+export default function VolumeSlider({ hiding }: { hiding?: boolean }) {
+  const [isActive, setIsActive] = useState(!hiding);
+  const toggleActive = () => {
+    hiding ? setIsActive((prevValue) => !prevValue) : null;
+  };
   const { volume, setVolume } = useStore((state) => state);
   const wrapperRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
+    hiding && document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   });
 
@@ -30,6 +32,7 @@ export default function VolumeSlider() {
     <div className="flex gap-4 items-center p-1" ref={wrapperRef}>
       <SpeakerOffIcon className={`${isActive ? " visible" : "hidden"}`} />
       <motion.form
+        initial={hiding ? { width: 0 } : { width: 200 }}
         animate={isActive ? { width: 200 } : { width: 0 }}
         transition={{ duration: 0.2 }}
         className="focus:outline outline-2"
@@ -38,6 +41,7 @@ export default function VolumeSlider() {
           className="relative flex items-center select-none touch-none h-5 w-full"
           defaultValue={[volume]}
           onValueChange={(values) => setVolume(values[0])}
+          value={[volume]}
           max={100}
           step={1}
         >
@@ -45,8 +49,8 @@ export default function VolumeSlider() {
             <Slider.Range className="absolute bg-white rounded-full h-full" />
           </Slider.Track>
           <Slider.Thumb
-            className={`block w-3 h-3 bg-white rounded-[10px] hover:bg-gray-300 transition-all focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA3 focus:bg-gray-300 ${
-              isActive ? "visible" : "hidden"
+            className={`block w-3 h-3 bg-white rounded-[10px] hover:bg-gray-300 transition-all focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA3 focus:bg-gray-300 visible ${
+              isActive ? "" : "hidden"
             }`}
             aria-label="Volume"
           />
