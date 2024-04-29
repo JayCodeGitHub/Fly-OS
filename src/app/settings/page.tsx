c4cea474d47760161103cb3d962df279ec994913
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-29 10:32:11 +0200

    Move settingsItems to separate file

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,29 +1,7 @@
 import Switch from "@/components/switch";
 import VolumeSlider from "@/components/volumeSlider";
 import Select from "@/components/select";
-
-const flights = [
-  {
-    value: "london - new york",
-    name: "London - New York",
-  },
-  {
-    value: "berlin - london",
-    name: "Berlin - London",
-  },
-  {
-    value: "new york - los angeles",
-    name: "New York - Los Angeles",
-  },
-  {
-    value: "dubai - berlin",
-    name: "Dubai - Berlin",
-  },
-  {
-    value: "los angeles - tokyo",
-    name: "Los Angeles - Tokyo",
-  },
-];
+import { flights } from "@/items/settingsItems";
 
 export default function Settings() {
   return (
---
+++ src/items/settingsItems.tsx
@@ -0,0 +1,22 @@
+export const flights = [
+  {
+    value: "london - new york",
+    name: "London - New York",
+  },
+  {
+    value: "berlin - london",
+    name: "Berlin - London",
+  },
+  {
+    value: "new york - los angeles",
+    name: "New York - Los Angeles",
+  },
+  {
+    value: "dubai - berlin",
+    name: "Dubai - Berlin",
+  },
+  {
+    value: "los angeles - tokyo",
+    name: "Los Angeles - Tokyo",
+  },
+];
