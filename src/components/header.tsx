c1306a8ded29edf16ddd9870625e115a93b4defc
JayCode Developer <JayCode.contakt@gmail.com>
2024-05-01 11:50:05 +0200

    Style header for Desktop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="flex flex-col gap-10">
+      <h1 className=" text-4xl">Fly OS</h1>
+      <h2 className=" text-3xl">Enjoy your flight</h2>
     </header>
   );
 }
