fa30c397dec7e1347ea527d9ccd48decd8ef14d5
JayCode Developer <JayCode.contakt@gmail.com>
2024-05-04 16:34:27 +0200

    Add structure to ProgressBar component

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,7 +1,11 @@
 export default function ProgressBar() {
   return (
     <div>
-      <h1>ProgressBar</h1>
+      <h3>Initial airport</h3>
+      <h3>Initial City</h3>
+      <span>Progress bar with airplane icon</span>
+      <h3>Destination airport</h3>
+      <h3>Destination City</h3>
     </div>
   );
 }
