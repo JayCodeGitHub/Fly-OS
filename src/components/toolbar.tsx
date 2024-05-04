c6c0e3af12b94a4727865274f90e9f2478d7862c
JayCode Developer <JayCode.contakt@gmail.com>
2024-05-04 16:07:26 +0200

    Style toolbar for mobile

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -2,7 +2,7 @@ import VolumeSlider from "./volumeSlider";
 
 export default function Toolbar() {
   return (
-    <div className="fixed top-0 left-0 h-28 px-48 w-screen flex justify-end items-center gap-12">
+    <div className="fixed top-0 left-0 h-28 xl:px-48 lg:px-24 px-8 w-screen flex justify-end items-center gap-12">
       <VolumeSlider hiding />
     </div>
   );
